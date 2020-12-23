4bf8650e90b4702ca1ed920a7e73ac1f5043b3a4
shivam907 <shivamkaushal907@gmail.com>
2020-12-23 16:26:11 +0530

    09

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,7 +16,7 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'cez.login@gmail.com'  #Email ID
+app.config['MAIL_USERNAME'] = 'cezlogin@gmail.com'  #Email ID
 app.config['MAIL_PASSWORD'] = 'creativeexpertz'  #Email Account Password
 mail = Mail(app)
 
