5545ea739a5ec7456356798a1e44f0e9222b3580
shivam907 <shivamkaushal907@gmail.com>
2020-12-24 10:28:23 +0530

    11

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -127,10 +127,6 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        # if form.img_file.data:
-            # img_file = postpics(form.img_file.data)
-            # db.session.add(form.img_file.data)
-            # img_file=form.img_file.data
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
